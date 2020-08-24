8ebafd087cf635a3a0fbc2fbba7164b078e32dee
MAX <40911673+Maxcs318@users.noreply.github.com>
2020-08-24 15:54:53 +0700

    footer

---
+++ client/src/components/Nav/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="footer bg-primary">
+        <div class="row">
+            <div class="col-12">
+                FOOTER Online : {{this.$store.getters.getInternet_status}}
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    .footer {
+        color: white;
+        text-align: center;
+        width: 100%;
+        position: fixed;
+         /* or absolute or relative*/
+        /* display: block; */
+        bottom: 0%;
+        margin:0;
+    }
+</style>
